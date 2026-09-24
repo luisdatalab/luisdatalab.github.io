84d9f140eda06a896000288511387b77e180f365
Luis Eduardo <165858483+EduardoMelonio@users.noreply.github.com>
2026-09-24 07:37:10 -0300

    update all

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,36 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1790245560',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/projetos/',
+      
+        '/about/',
+      
+        '/formacao/',
+      
+        '/contato/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
